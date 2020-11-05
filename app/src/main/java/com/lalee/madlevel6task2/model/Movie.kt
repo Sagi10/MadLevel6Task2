6ab0add0b43271a8cbffe15434a1a4c1b558c6ff
Sagi <sagi.lalee@hva.nl>
2020-11-05 11:22:44 +0100

    added Movie model

--- app/src/main/java/com/lalee/madlevel6task2/model/Movie.kt
+++ app/src/main/java/com/lalee/madlevel6task2/model/Movie.kt
@@ -1,4 +1,13 @@
 package com.lalee.madlevel6task2.model
 
-class Movie {
-}
+import com.google.gson.annotations.SerializedName
+
+
+data class Movie(
+    @SerializedName("title")var title: String,
+    @SerializedName("release_date") var releaseDate: String,
+    @SerializedName("vote_average") var rating: Int,
+    @SerializedName("overview")var overview: String,
+    @SerializedName("backdrop_path") var imageBackdrop: String,
+    @SerializedName("poster_path") var imagePost: String
+)
